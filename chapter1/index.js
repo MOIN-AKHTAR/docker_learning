3dcb889cb8154598dd30f8efe19718caa36c4970
Moin Akhter <47149401+MOIN-AKHTAR@users.noreply.github.com>
2022-12-01 22:51:12 +0500

    Created dockerfile inorder to create image and than run container based on created image

---
+++ chapter1/index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+
+const app=express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+
+    return res.json({
+        message: 'Hello World!'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
